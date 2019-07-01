6afcafde6c927279fab84760bc62b3e68b84dbf2
Maksim Sukhotski <maksim.sukhotski@consultant.turkcell.com.tr>
2019-07-01 17:27:40 +0300

    Implement coloring square

---
+++ app/src/main/java/com/suhocki/algorithms/ColoringABorder.kt
@@ -0,0 +1,96 @@
+package com.suhocki.algorithms
+
+/**
+ * https://leetcode.com/problems/coloring-a-border/
+ */
+class ColoringABorder {
+    fun colorBorder(grid: Array<IntArray>, r0: Int, c0: Int, color: Int): Array<IntArray> {
+        val ourColor = grid[r0][c0]
+        val yMax = grid.lastIndex
+        val xMax = grid.first().lastIndex
+        val isBorder = Array(grid.size) { BooleanArray(grid.first().size) }
+        val traversedRight = Array(grid.size) { BooleanArray(grid.first().size) }
+        val traversedBottom = Array(grid.size) { BooleanArray(grid.first().size) }
+        val traversedLeft = Array(grid.size) { BooleanArray(grid.first().size) }
+        val traversedTop = Array(grid.size) { BooleanArray(grid.first().size) }
+        var i = r0
+        var j = c0
+
+        // region Traverse to top
+        /**
+         * Traverse to top
+         */
+        var wasInTraverseToTopLoop = false
+        while (i in 0..yMax && j in 0..xMax && grid[i][j] == ourColor && !traversedTop[i][j]) {
+            wasInTraverseToTopLoop = true
+            // region Traverse to left
+            /**
+             * Traverse to left
+             */
+            var wasInTraverseToLeftLoop = false
+            while (i in 0..yMax && j in 0..xMax && grid[i][j] == ourColor && !traversedLeft[i][j]) {
+                wasInTraverseToLeftLoop = true
+                // region Traverse to bottom
+                /**
+                 * Traverse to bottom
+                 */
+                var wasInTraverseToBottomLoop = false
+                while (i in 0..yMax && j in 0..xMax && grid[i][j] == ourColor && !traversedBottom[i][j]) {
+                    wasInTraverseToBottomLoop = true
+                    // region Traverse to right
+                    /**
+                     * Traverse to right
+                     */
+                    var wasInTraverseToRightLoop = false
+                    while (i in 0..yMax && j in 0..xMax && grid[i][j] == ourColor && !traversedRight[i][j]) {
+                        wasInTraverseToRightLoop = true
+                        traversedRight[i][j] = true
+                        j++
+                    }
+                    if (wasInTraverseToRightLoop) {
+                        j--
+                    }
+                    isBorder[i][j] = true
+                    // endregion
+
+                    traversedBottom[i][j] = true
+                    i++
+                }
+                if (wasInTraverseToBottomLoop) {
+                    i--
+                }
+                isBorder[i][j] = true
+                // endregion
+
+                traversedLeft[i][j] = true
+                j--
+            }
+            if (wasInTraverseToLeftLoop) {
+                j++
+            }
+            isBorder[i][j] = true
+            // endregion
+
+            traversedTop[i][j] = true
+            i--
+        }
+        if (wasInTraverseToTopLoop) {
+            i++
+        }
+        isBorder[i][j] = true
+        // endregion
+
+        /**
+         * Color border
+         */
+        for (i in 0..yMax) {
+            for (j in 0..xMax) {
+                if (isBorder[i][j]) {
+                    grid[i][j] = color
+                }
+            }
+        }
+
+        return grid
+    }
+}
---
+++ app/src/test/java/com/suhocki/algorithms/ColoringABorderTest.kt
@@ -0,0 +1,87 @@
+package com.suhocki.algorithms
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ColoringABorderTest {
+    private val pen = ColoringABorder()
+
+    @Test
+    fun test1() {
+        // Given
+        val grid = arrayOf(
+            intArrayOf(1, 1),
+            intArrayOf(1, 2)
+        )
+        val r0 = 0
+        val c0 = 0
+        val color = 3
+
+        // When
+        val answer = pen.colorBorder(grid, r0, c0, color)
+
+        // Then
+        assert(
+            answer.contentDeepEquals(
+                arrayOf(
+                    intArrayOf(3, 3),
+                    intArrayOf(3, 2)
+                )
+            )
+        )
+    }
+
+    @Test
+    fun test2() {
+        // Given
+        val grid = arrayOf(
+            intArrayOf(1, 2, 2),
+            intArrayOf(2, 3, 2)
+        )
+        val r0 = 0
+        val c0 = 1
+        val color = 3
+
+        // When
+        val answer = pen.colorBorder(grid, r0, c0, color)
+
+        // Then
+        assert(
+            answer.contentDeepEquals(
+                arrayOf(
+                    intArrayOf(1, 3, 3),
+                    intArrayOf(2, 3, 3)
+                )
+            )
+        )
+    }
+
+    @Test
+    fun test3() {
+        // Given
+        val grid = arrayOf(
+            intArrayOf(1, 1, 1, 1),
+            intArrayOf(1, 1, 1, 1),
+            intArrayOf(1, 1, 1, 1),
+            intArrayOf(1, 1, 1, 1)
+        )
+        val r0 = 1
+        val c0 = 1
+        val color = 2
+
+        // When
+        val answer = pen.colorBorder(grid, r0, c0, color)
+
+        // Then
+        assert(
+            answer.contentDeepEquals(
+                arrayOf(
+                    intArrayOf(2, 2, 2),
+                    intArrayOf(2, 1, 2),
+                    intArrayOf(2, 2, 2)
+                )
+            )
+        )
+    }
+}
